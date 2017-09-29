7584a3e0fd97d9401fb80da66720f2e9765a91e1
9Kevinzhao <9kzhao@stpatrick.org>
2017-09-29 14:45:42 -0500

    Add files via upload

---
+++ testlinkedlist.js
@@ -0,0 +1,11 @@
+LinkedList=require('./LinkedList.js');
+LinkedList=require('./node.js');
+
+function main(){
+  let n1=new Node(1);
+  let n2=new Node(2);
+  let mylist= new LinkedList();
+  mylist.add(n1);
+  mylist.add(n2);
+
+}
